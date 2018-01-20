418b9679a6284e142177306e993ef1a079edc033
Dalimil <dalimilhajek@gmail.com>
2018-01-20 14:40:35 +0000

    Add basic chat interface

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.speak('Hello World');
+  }
+
+  speak(text) {
+    window.responsiveVoice.speak(text, "UK English Male");
+  }
+
+  getChatbotReply(text) {
+    return "cool";
+  }
+
+  ask(text) {
+    if (!text) {
+      console.warn("you didn't say anything");
+      return;
+    }
+    const response = this.getChatbotReply(text);
+    this.speak(response);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div id="message">
+          <h2>Welcome FC Bayern Fans</h2>
+          <h1>Talk to us...</h1>
+          <textarea ref={node => this.textareaNode = node} autofocus></textarea>
+          <p>Your football player will talk back...</p>
+          <a href="#" onClick={() => this.ask(this.textareaNode.value)}>Speak</a>
+        </div>
+        <p id="load">Thank you for supporting us. FC Bayern Munich</p>
       </div>
     );
   }
