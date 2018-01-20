523bf84d4479a0edd332861f884f6492004991b1
Dalimil <dalimilhajek@gmail.com>
2018-01-20 16:52:36 +0000

    Integrate chatbots

--- src/App.js
+++ src/App.js
@@ -8,32 +8,49 @@ class App extends Component {
     this.speak('Hello World');
   }
 
-  speak(text) {
-    window.responsiveVoice.speak(text, "UK English Male");
+  speak(text, alternativeVoice) {
+    return new Promise(resolve => {
+      const config = {
+        //pitch: 2,
+        volume: 2,
+        onend: resolve
+      };
+      window.responsiveVoice.speak(text, alternativeVoice ? "UK English Female" : "UK English Male", config);
+    });
   }
 
   getChatbotReply(text) {
-    return "cool";
+    return fetch(`https://api.dialogflow.com/v1/query?v=20170712&query=${window.encodeURI(text)}&lang=en&sessionId=494475ee-8fb6-45ef-8d8a-bcdd255910a6&timezone=Europe/London`, {
+      headers: new Headers({
+        'Authorization': 'Bearer f1b3acd841674614b971cf20790cb531'
+      })
+    })
+    .then(x => x.json())
+    .then(j => j.result.fulfillment.speech);
   }
 
-  ask(text) {
+  ask(textareaNode) {
+    const text = textareaNode.value;
     if (!text) {
       console.warn("you didn't say anything");
       return;
     }
+    textareaNode.value = "";
     const response = this.getChatbotReply(text);
-    this.speak(response);
+    this.speak(text, false).then(() => {
+      response.then(toSay => this.speak(toSay, true));
+    });
   }
-
+  
   render() {
     return (
       <div>
         <div id="message">
           <h2>Welcome FC Bayern Fans</h2>
           <h1>Talk to us...</h1>
-          <textarea ref={node => this.textareaNode = node} autofocus></textarea>
+          <textarea ref={node => this.textareaNode = node} autoFocus></textarea>
           <p>Your football player will talk back...</p>
-          <a href="#" onClick={() => this.ask(this.textareaNode.value)}>Speak</a>
+          <a href="#" onClick={() => this.ask(this.textareaNode)}>Speak</a>
         </div>
         <p id="load">Thank you for supporting us. FC Bayern Munich</p>
       </div>
